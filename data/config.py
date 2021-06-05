ffe994b37d7510a9f7f640e19ced31a15fd5374d
git_azamat <almazbek1608@gmail.com>
2021-06-05 13:57:05 +0200

    token

--- data/config.py
+++ data/config.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # BOT_TOKEN = str(os.getenv('BOT_TOKEN'))
-# BOT_TOKEN = '1396981907:AAEfXRYucqVgChkRH57X_6xCSuWwT6QPmqY'  # KONNEKT
-BOT_TOKEN = '1516243018:AAH1r7kMgzOe9oArCYPRWbMY_p2kVDmhwbA'  # New test bot
+BOT_TOKEN = '1396981907:AAEfXRYucqVgChkRH57X_6xCSuWwT6QPmqY'  # KONNEKT
+# BOT_TOKEN = '1516243018:AAH1r7kMgzOe9oArCYPRWbMY_p2kVDmhwbA'  # New test bot
 
 admins = [
     268388996
