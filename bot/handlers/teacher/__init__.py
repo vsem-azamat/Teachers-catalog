c71a84bb2a2d2a4bdd224a70ea1a5cc328cd3782
poryadok <almazbek1608@gmail.com>
2023-12-01 15:20:35 +0100

    -

--- bot/handlers/teacher/__init__.py
+++ bot/handlers/teacher/__init__.py
@@ -2,8 +2,10 @@ from aiogram import Router
 
 from .teacher_registration import router as teacher_registration_router
 from .teacher_settings import router as teacher_settings_router
+from .teacher_lessons import router as teacher_lessons_router
 
 router = Router()
 
 router.include_router(teacher_registration_router)
 router.include_router(teacher_settings_router)
+router.include_router(teacher_lessons_router)
