bc60faebb69d19135241815fca5160df44290992
vsem-azamat <almazbek1608@gmail.com>
2024-02-07 16:56:47 +0100

    new assert

--- bot/config.py
+++ bot/config.py
@@ -17,9 +17,7 @@ class SettingsDB:
     DATABASE: str = config('DB_DATABASE', default=None)
     PORT: int = int(config('DB_PORT', cast=int, default=5432))
 
-
-    if not USERNAME or not PASSWORD or not HOST or not DATABASE or not PORT:
-        raise Exception('DB settings are not set')
+    assert USERNAME and PASSWORD and HOST and DATABASE and PORT, 'DB settings are not set'
 
 
 class CatalogConfig:
